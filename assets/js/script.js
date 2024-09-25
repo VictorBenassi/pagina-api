482edc4d285fa4d61fe4bb0654fd676159ec6287
Arthur Lima Cogo <67808884+ArthurLimaCogo@users.noreply.github.com>
2024-09-25 19:25:15 -0300

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,25 @@ document.addEventListener('DOMContentLoaded', () => {
     // Ajuste inicial
     adjustHeight();
 });
+
+// Mostrar ou ocultar a seção de FAQ e o fundo ao clicar em "Ajuda"
+document.getElementById('help-link').addEventListener('click', function(e) {
+    e.preventDefault();
+    const faqSection = document.getElementById('faq-section');
+    const backdrop = document.getElementById('backdrop');
+    
+    if (faqSection.style.display === 'none' || faqSection.style.display === '') {
+      faqSection.style.display = 'block';
+      backdrop.style.display = 'block'; // Mostrar o fundo
+    } else {
+      faqSection.style.display = 'none';
+      backdrop.style.display = 'none'; // Esconder o fundo
+    }
+  });
+  
+  // Fechar a FAQ e o backdrop se o fundo for clicado
+  document.getElementById('backdrop').addEventListener('click', function() {
+    document.getElementById('faq-section').style.display = 'none';
+    document.getElementById('backdrop').style.display = 'none';
+  });
+  
