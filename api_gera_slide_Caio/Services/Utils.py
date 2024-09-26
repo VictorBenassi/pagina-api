dd81091a9cb34f8dccb7c08c50d9829295f97b07
VictorBenassi <victorbenassi0@gmail.com>
2024-09-26 17:07:03 -0300

    Delete dos testes API

--- api_gera_slide_Caio/Services/Utils.py
+++
@@ -1,8 +0,0 @@
-import json
-
-class Utils:
-    @staticmethod
-    def GetCredentials() -> dict:
-        with open(r'Data/appConfig.json', 'r') as file:
-            jsonString = file.read()
-            return json.loads(jsonString)
